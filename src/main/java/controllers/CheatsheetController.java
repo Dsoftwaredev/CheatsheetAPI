ad22f5c9bab12f6e4a9263c521adb2f1d7098fb5
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-12 20:03:02 +0100

    update cheat working

--- src/main/java/controllers/CheatsheetController.java
+++ src/main/java/controllers/CheatsheetController.java
@@ -1,11 +1,9 @@
 package controllers;
 
 import entities.Cheatsheet;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,11 @@ public class CheatsheetController {
         return this.service.createCheat(cheat);
     }
 
+    @PutMapping("/cheats/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Cheatsheet updateCheat(@PathVariable("id") Long id, @RequestBody Cheatsheet cheat) {
+        return this.service.updateCheat(id, cheat);
+
+    }
 
 }
--- src/main/java/controllers/CheatsheetService.java
+++ src/main/java/controllers/CheatsheetService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import repositories.CheatsheetRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CheatsheetService {
@@ -22,5 +23,28 @@ public class CheatsheetService {
     public Cheatsheet createCheat(Cheatsheet cheat) {
         Cheatsheet newCheatsheet = this.repo.save(cheat);
         return newCheatsheet;
+    }
+
+    public Cheatsheet updateCheat(Long id, Cheatsheet cheat) {
+        Optional<Cheatsheet> CheatToUpdateOptional = this.repo.findById(id);
+        if (!CheatToUpdateOptional.isPresent()) {
+            return null;
+        }
+
+        // Since isPresent() was true, we can .get() the Person object out of the Optional
+        Cheatsheet CheatToUpdate = CheatToUpdateOptional.get();
+
+        if (cheat.getCodeLanguage() != null) {
+            CheatToUpdate.setCodeLanguage(cheat.getCodeLanguage());
+        }
+        if (cheat.getCheatMethod() != null) {
+            CheatToUpdate.setCheatMethod(cheat.getCheatMethod());
+        }
+        if (cheat.getCheatBody() != null) {
+            CheatToUpdate.setCheatBody(cheat.getCheatBody());
+        }
+        Cheatsheet updatedCheat = this.repo.save(CheatToUpdate);
+        return updatedCheat;
+
     }
 }
