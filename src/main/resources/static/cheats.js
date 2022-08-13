4261fe17def07b7fe1799d0ec583aa87a9d6ce18
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-08-13 18:00:44 +0100

    full crud on front end for cheats

--- src/main/resources/static/cheats.js
+++ src/main/resources/static/cheats.js
@@ -46,3 +46,51 @@ fetch(`/cheats/${deleteId}`, {
 .then(data => console.log(data))
 .catch(error => console.log(error))
 }
+
+const updateCheat = (e) => {
+let id = document.getElementById('updateid').value;
+let languageone = document.querySelector("#updatename").value
+let  chMethodtwo= document.querySelector('#updatemethod').value
+let chBodythree = document.querySelector('#updateBody').value
+    fetch(`/cheats/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(
+            {
+                "codeLanguage": languageone,
+                                "cheatMethod": chMethodtwo,
+                                "cheatBody": chBodythree
+        }),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res => {
+        res.json().then(body => {
+            document.body.innerHTML = JSON.stringify(body)
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
+const renderRawResponse = (res) => {
+  // Takes the first 10 words from res
+  let response = res;
+  // Manipulates responseField to render the unformatted response
+  responseField.innerHTML = `<text>${JSON.stringify(response)}</text>`;
+}
+
+
+const getCheats = () => {
+const responseField = document.querySelector('#responseField');
+const cheats = document.querySelector('#cheats')
+  fetch('/cheats').then(response => {
+console.log('teststock')
+      response.json().then(jsonResponse => {
+                               renderRawResponse(jsonResponse);
+                              //  renderResponse(jsonResponse);
+                              })
+
+  }).catch( networkError => {
+              console.log(networkError.message)
+            })
+}
